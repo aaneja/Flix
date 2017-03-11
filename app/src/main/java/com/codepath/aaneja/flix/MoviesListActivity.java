96da600c95eb82e90c1e37c53696a4e1c8a1c346
aaneja <anant.aneja@gmail.com>
2017-03-11 11:35:02 -0800

    - Wrapped the MovieDb Facade call in an AsyncTask - Tested and the app loads up and fetches information and displays overviews correctly

--- app/src/main/java/com/codepath/aaneja/flix/MoviesListActivity.java
+++ app/src/main/java/com/codepath/aaneja/flix/MoviesListActivity.java
@@ -1,11 +1,18 @@
 package com.codepath.aaneja.flix;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 
 import com.codepath.aaneja.flix.adapters.MovieItemAdapter;
+import com.codepath.aaneja.flix.models.MovieItem;
 import com.codepath.aaneja.flix.services.MovieDbFacade;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import static com.codepath.aaneja.flix.R.id.lvItems;
 
@@ -16,11 +23,27 @@ public class MoviesListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies_list);
 
-        //Fetch the movies
-        MovieDbFacade movieDbFacade = new MovieDbFacade();
         ListView lvItems = (ListView) findViewById(R.id.lvItems);
-        MovieItemAdapter movieItemAdapter = new MovieItemAdapter(this, R.layout.activity_movies_list, movieDbFacade.GetNowPlaying());
-        lvItems.setAdapter(movieItemAdapter);
+
+        AsyncTask<Context,Integer,MovieItemAdapter> asyncTask = new AsyncTask<Context,Integer,MovieItemAdapter>() {
+            @Override
+            protected MovieItemAdapter doInBackground(Context... params) {
+                Context caller = params[0];
+                MovieDbFacade movieDbFacade = new MovieDbFacade();
+                List<MovieItem> nowPlayingMovies = movieDbFacade.GetNowPlaying();
+                return new MovieItemAdapter(caller, R.layout.activity_movies_list,nowPlayingMovies);
+            }
+        };
+
+
+        try {
+            AsyncTask<Context, Integer, MovieItemAdapter> task = asyncTask.execute(this);
+            lvItems.setAdapter(task.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
 
     }
 
