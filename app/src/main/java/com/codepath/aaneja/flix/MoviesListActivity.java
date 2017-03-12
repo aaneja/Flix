e7aaa49d5edf57c6e3ad888eaa23945a62acd2aa
aaneja <anant.aneja@gmail.com>
2017-03-11 19:02:04 -0800

    - Changed asyncTask execution to not block the UI thread - Changed text to show ellipsis

--- app/src/main/java/com/codepath/aaneja/flix/MoviesListActivity.java
+++ app/src/main/java/com/codepath/aaneja/flix/MoviesListActivity.java
@@ -11,7 +11,6 @@ import com.codepath.aaneja.flix.models.MovieItem;
 import com.codepath.aaneja.flix.services.MovieDbFacade;
 
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 public class MoviesListActivity extends Activity {
 
@@ -19,8 +18,6 @@ public class MoviesListActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies_list);
-
-        ListView lvItems = (ListView) findViewById(R.id.lvItems);
 
         AsyncTask<Context,Integer,MovieItemAdapter> asyncTask = new AsyncTask<Context,Integer,MovieItemAdapter>() {
             @Override
@@ -30,18 +27,15 @@ public class MoviesListActivity extends Activity {
                 List<MovieItem> nowPlayingMovies = movieDbFacade.GetNowPlaying();
                 return new MovieItemAdapter(caller, R.layout.activity_movies_list,nowPlayingMovies);
             }
+
+            @Override
+            protected void onPostExecute(MovieItemAdapter movieItemAdapter) {
+                ListView lvItems = (ListView) findViewById(R.id.lvItems);
+                lvItems.setAdapter(movieItemAdapter);
+            }
         };
 
-
-        try {
-            AsyncTask<Context, Integer, MovieItemAdapter> task = asyncTask.execute(this);
-            lvItems.setAdapter(task.get());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-
+        asyncTask.execute(this);
     }
 
 }
